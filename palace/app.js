729f6abe590cc8ae288a948013e98b1a22c0ac9f
Victor Tang <victortang1992@gmail.com>
2016-03-31 23:14:30 -0700

    sidescrolling

--- palace/app.js
+++ palace/app.js
@@ -1013,7 +1013,51 @@ app.controller('MainCtrl', ['$scope', '$q', '$timeout', 'DATASTORE', function($s
 	};
 	
 }]);//end of controller
+
+var position = -50;//current position
 	
+app.directive('ngSideScroll', function() {
+        return function(scope, element, attrs) {
+            element.bind("DOMMouseScroll mousewheel onmousewheel", function(event) {
+
+                        // cross-browser wheel delta
+                        var event = window.event || event; // old IE support
+                        var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
+                        var base = -50; //base position
+						var hiBound = base + (element[0].clientWidth/document.body.clientWidth-1)*50;
+						var lowBound = base - (element[0].clientWidth/document.body.clientWidth-1)*50;
+
+						//only works if element is wider than screen
+						if(element[0].clientWidth > document.body.clientWidth){
+							if(delta > 0) {
+								//see more towards left (<____-3____-2____-1____0)
+								//if new position would still be less than hibound
+								if(position+delta < hiBound){
+									position+=delta;
+								}
+							}
+							else if (delta < 0){
+								//see more towards right (0____1____2____3____>)
+								//if new position would still be more than lobound
+								if(lowBound < position+delta){
+									position+=delta;
+								}
+							}
+
+							element[0].style.webkitTransform = 'translateX('+position+'%)';
+							element[0].style.mozTransform = 'translateX('+position+'%)';
+							element[0].style.transform = 'translateX('+position+'%)';
+						}
+						// for IE
+						event.returnValue = false;
+					 	// for Chrome and Firefox
+					 	if(event.preventDefault) {
+					  		event.preventDefault();
+					 	}
+            });
+        };
+});
+
 Array.prototype.allValuesSame = function() {
 
     for(var i = 1; i < this.length; i++)
